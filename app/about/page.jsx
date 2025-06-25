19abb7e6e49bebb2ee61afb00743d4e0a02cf707
Samson Sompien Daniel <theafricanchild19@gmail.com>
2025-06-25 16:39:15 +0100

    Removed some component in the page file

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,27 +1,10 @@
-import AboutHero from "@/components/AboutHero"
-import Footer from "/components/Footer"
 import Navigation from "/components/Navigation"
-import HelpSection from "@/components/HelpSection"
-import ThankYouCarousel from "@/components/ThankYouCarousel"
-import PartnersSection from "@/components/PartnersSection"
-import OurTeam from "@/components/OurTeam"
-import HelpingHands from "@/components/HelpingHands"
-import ProgramsSection from "@/components/ProgramsSection"
-import AboutStatement from "@/components/AboutStatement"
+
 
 const About = () => {
   return (
     <div>
         <Navigation />
-        <AboutHero />
-        <HelpingHands />
-        <AboutStatement />
-        <HelpSection />
-        <ProgramsSection />
-        <ThankYouCarousel />
-        <PartnersSection />
-        <OurTeam />
-        <Footer />
     </div>
   )
 }
--- app/programs/page.jsx
+++ app/programs/page.jsx
@@ -1,21 +1,11 @@
-import ProgramHero from "@/components/ProgramHero"
-import Footer from "/components/Footer"
 import Navigation from "/components/Navigation"
-import HelpingHands from "@/components/HelpingHands"
-import ProgramDescription from "@/components/ProgramDescription"
-import HelpSection from "@/components/HelpSection"
-import Section from "@/components/Section"
+
 
 const Programs = () => {
   return (
     <div>
         <Navigation />
-        <ProgramHero />
-        <HelpingHands />
-        <ProgramDescription />
-        <HelpSection />
-        <Section />
-        <Footer />
+        
     </div>
   )
 }
